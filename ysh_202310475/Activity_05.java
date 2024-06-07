dbaa01274625b74f54c4a0a74e7a4e9e048a97c3
yangfaring <did030930@gmail.com>
2024-06-07 20:41:10 +0900

    Update Activity_05.java

--- ysh_202310475/Activity_05.java
+++ ysh_202310475/Activity_05.java
@@ -30,24 +30,24 @@ public class Activity_05 extends Activity {
         button22 = (Button) findViewById(R.id.button22);
         textView13=(TextView) findViewById(R.id.textView13);
         textView14=(TextView) findViewById(R.id.textView14);
-
+        //계산하기 버튼
         button20.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if ((editTextNumber7.getText().length() != 0) && (editTextNumber9.getText().length() != 0) && (editTextNumber10.getText().length() != 0)) {
-                    num1 = Integer.parseInt(editTextNumber7.getText().toString());
-                    num2 = Integer.parseInt(editTextNumber9.getText().toString());
-                    num3 = Integer.parseInt(editTextNumber10.getText().toString());
-                    int result1 = num1 + num2 + num3;
-                    result= (15000*num1)+(13000*num2)+(9000*num3);
+                    num1 = Integer.parseInt(editTextNumber7.getText().toString()); //피자 개수
+                    num2 = Integer.parseInt(editTextNumber9.getText().toString()); //스파게티 개수
+                    num3 = Integer.parseInt(editTextNumber10.getText().toString()); //샐러드 개수
+                    int result1 = num1 + num2 + num3; //주문 개수
+                    result= (15000*num1)+(13000*num2)+(9000*num3); // 주문 금액
                     textView13.setText(" 개수: " + result1);
-                    textView14.setText("결과 :" + result.toString());
+                    textView14.setText(" 금액 :" + result.toString()); // xml 텍스트칸 주의하기
                 } else{
                     Toast.makeText(getApplicationContext(), "똑바로 입력해라",Toast.LENGTH_LONG).show();
                 }
             }
         });
-
+        //초기화 버튼
         button21.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,6 +56,7 @@ public class Activity_05 extends Activity {
                 editTextNumber10.setText("");
             }
         });
+        //돌아가기 버튼
         button22.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
